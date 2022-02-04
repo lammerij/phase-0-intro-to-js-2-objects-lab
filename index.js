0e634b2d74d349656a784bc54903305e4e930010
+ <+>
2022-02-04 17:22:14 -0500

    commit tree 5c722e36eaf7c996dc14e2ce2ddc88017d0f4300

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee = {
+  name: "Sam",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let newEmployee = { ...employee, [key]: value };
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  console.log({ ...employee, [key]: value });
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  console.log({ ...employee });
+  let newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  console.log({ ...employee, key });
+  let newEmployee = { ...employee, key };
+  delete employee[key];
+  return employee;
+}
